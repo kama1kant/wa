832c5111bee89da230db8c09a65d659a2bd1ad94
kama1kant <kamalkant.k4@gmail.com>
2023-03-21 16:41:40 -0700

    Updates

---
+++ precache-manifest.7bf4d2fcdc966175c358f71deecf9e79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "faee54871b35b8797b9b",
+    "url": "/static/css/main.d6ffdea0.chunk.css"
+  },
+  {
+    "revision": "faee54871b35b8797b9b",
+    "url": "/static/js/main.faee5487.chunk.js"
+  },
+  {
+    "revision": "346e52cedf411064dd72",
+    "url": "/static/js/1.346e52ce.chunk.js"
+  },
+  {
+    "revision": "dc13e5dc18dcf45b8b2b",
+    "url": "/static/css/2.4e5642a7.chunk.css"
+  },
+  {
+    "revision": "dc13e5dc18dcf45b8b2b",
+    "url": "/static/js/2.dc13e5dc.chunk.js"
+  },
+  {
+    "revision": "bfe694c59b4143d0770c",
+    "url": "/static/js/runtime~main.bfe694c5.js"
+  },
+  {
+    "revision": "844324dc83d98980285a9ab8f3dcb5f0",
+    "url": "/static/media/Aeonik-Regular.844324dc.otf"
+  },
+  {
+    "revision": "91e0638dac19aba2539d2b8b2a7981e8",
+    "url": "/static/media/GT-Walsheim-Bold.91e0638d.ttf"
+  },
+  {
+    "revision": "44e338f93b7f4b5b5520145f8d0a496a",
+    "url": "/static/media/solys-logo-white.44e338f9.svg"
+  },
+  {
+    "revision": "2b9868512b22fb14044731b36f7e5d2e",
+    "url": "/static/media/mockup.2b986851.svg"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "d2c876f11dea8b795880ca8c6b10fd60",
+    "url": "/index.html"
+  }
+];
